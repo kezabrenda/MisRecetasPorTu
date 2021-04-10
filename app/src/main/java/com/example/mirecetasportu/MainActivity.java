48cd8b7645e5abf0f0c89d77f4ff657d4634a205
keza1brenda <keza1brenda@gmail.com>
2021-04-10 08:03:28 +0200

    add first layout

--- app/src/main/java/com/example/mirecetasportu/MainActivity.java
+++ app/src/main/java/com/example/mirecetasportu/MainActivity.java
@@ -2,13 +2,31 @@ package com.example.mirecetasportu;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+    @BindView(R.id.findRecipesButton) Button mFindRecipesButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ButterKnife.bind(this);
+
+        mFindRecipesButton = (Button) findViewById(R.id.findRecipesButton);
+        mFindRecipesButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, RecipesActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
