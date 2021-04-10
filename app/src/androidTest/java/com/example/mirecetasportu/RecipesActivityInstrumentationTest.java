56d9e9445ccb58467ebc8e47fff60c1329309b66
keza1brenda <keza1brenda@gmail.com>
2021-04-10 10:26:39 +0200

    add first test

--- app/src/androidTest/java/com/example/mirecetasportu/RecipesActivityInstrumentationTest.java
+++ app/src/androidTest/java/com/example/mirecetasportu/RecipesActivityInstrumentationTest.java
@@ -1,4 +1,52 @@
 package com.example.mirecetasportu;
 
+import android.view.View;
+
+import androidx.test.core.app.ActivityScenario;
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static androidx.test.espresso.Espresso.onData;
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.RootMatchers.withDecorView;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.anything;
+import static org.hamcrest.Matchers.not;
+
+@RunWith(AndroidJUnit4.class)
 public class RecipesActivityInstrumentationTest {
+    @Rule
+    public ActivityScenarioRule<RecipesActivity> activityTestRule =
+            new ActivityScenarioRule<>(RecipesActivity.class);
+
+    private View activityDecorView;
+
+    @Before
+    public void setUp() {
+        activityTestRule.getScenario().onActivity(new ActivityScenario.ActivityAction<RecipesActivity>() {
+            @Override
+            public void perform(RecipesActivity activity) {
+                activityDecorView = activity.getWindow().getDecorView();
+            }
+        });
+    }
+
+    @Test
+    public void listItemClickDisplaysToastWithCorrectRecipe() {
+        String dessertName = "Jell-O";
+        onData(anything())
+                .inAdapterView(withId(R.id.listView))
+                .atPosition(0)
+                .perform(click());
+        onView(withText(dessertName)).inRoot(withDecorView(not(activityDecorView)))
+                .check(matches(withText(dessertName)));
+    }
 }
